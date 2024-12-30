d5ad0eb6a79d34d058c867bbe7fc1e880f091276
michael faith <michaelfaith@users.noreply.github.com>
2024-12-30 09:04:30 -0600

    refactor: migrate to TypeScript
    This change migrates the project to TypeScript.  Beyond providing types now, as well as an ESM option, the aim was to not change the API in a breaking way.
    
    In doing this migration, I actually found at least one bug (TypeScript doing its job!).  In `validate`, `getSpecMap` was called, and immediately after there was an if condition to check if the result that came back was `false`, which would indicate the requested spec name was bad.  However the if condition checked if `specName` was false, rather than the result of the `getSpecMap` function.  That would have allowed people to pass in nonsense specNames and throwing an error later when attempting to access `map`.  I've fixed that here.
    
    I did put a TODO in though to consider deprecating the one named PJV export, in favor of directly exporting the functions and constants.  This would make the API more tree-shakable and (in my opinion) more straightforward.  But that should be a separate change, if implemented.
    
    The other quality of life stuff I adjusted:
    - Added TsDoc in more places
    - Changed concatenated strings to template strings

--- src/bin/pjv.ts
+++ src/bin/pjv.ts
@@ -1,13 +1,27 @@
 #!/usr/bin/env node
-/* Command line interface for package-json-validator.
- *
+/**
+ * Command line interface for package-json-validator.
  * For command line options, try --help
-
  * See README.md for more information
  */
+import fs from "node:fs";
+import process from "node:process";
+
+import yargs from "yargs";
+
+import { PJV } from "../PJV.js";
+import type { SpecName } from "../types";
+
+type Options = {
+	filename: string;
+	spec: SpecName;
+	warnings: boolean;
+	recommendations: boolean;
+	quiet: boolean;
+};
 
 // Command line options
-var options = require("yargs/yargs")(process.argv.slice(2))
+const options = yargs(process.argv.slice(2))
 	.options("filename", {
 		default: "package.json",
 		alias: "f",
@@ -16,6 +30,7 @@ var options = require("yargs/yargs")(process.argv.slice(2))
 	.options("spec", {
 		default: "npm",
 		alias: "s",
+		choices: ["npm", "commonjs_1.0", "commonjs_1.1"],
 		description: "spec to use - npm|commonjs_1.0|commonjs_1.1",
 	})
 	.options("warnings", {
@@ -37,28 +52,25 @@ var options = require("yargs/yargs")(process.argv.slice(2))
 		description: "less output",
 	})
 	.usage("Validate package.json files")
-	.parse();
+	.parse() as Options;
 
-var PJV = require("../PJV").PJV;
-var fs = require("fs");
 if (!fs.existsSync(options.filename)) {
 	console.error("File does not exist: " + options.filename);
-	process.exit(1);
-}
-
-var contents = fs.readFileSync(options.filename).toString(),
-	results = PJV.validate(contents, options.spec, {
-		warnings: options.warnings,
-		recommendations: options.recommendations,
-	});
-
-if (results.valid) {
-	if (!options.quiet) {
-		console.log(results);
-	}
-	process.exit(0);
+	process.exitCode = 1;
 } else {
-	console.error(options.filename + " is NOT valid");
-	console.error(results);
-	process.exit(1);
+	const contents = fs.readFileSync(options.filename).toString(),
+		results = PJV.validate(contents, options.spec, {
+			warnings: options.warnings,
+			recommendations: options.recommendations,
+		});
+
+	if (results.valid) {
+		if (!options.quiet) {
+			console.log(results);
+		}
+	} else {
+		console.error(options.filename + " is NOT valid");
+		console.error(results);
+		process.exitCode = 1;
+	}
 }
