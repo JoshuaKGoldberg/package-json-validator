e75d451a427c38bde871ca6b4dfe496bc7af3088
michael faith <michaelfaith@users.noreply.github.com>
2025-05-31 15:45:02 -0500

    build: bundle build files
    This change is focused on optimizing our package, and to address a known bug with how we were handling support for both cjs and esm.
    
    With this change, we're now bundling the two main entry points:
    - the root of our js/ts api
    - the cli bin
    
    As part of multi-entry point best practices, I changed the import of `validate` in the second entry-point to references the package, rather than relative path, so we weren't doubling up on the bundles.
    
    Package size (compressed) before:
    Package size (compressed) after: 15kb

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-	clean: true,
-	dts: true,
-	entry: ["src/index.ts", "src/bin/pjv.ts"],
-	external: ["package-json-validator"],
-	format: ["cjs", "esm"],
-	outDir: "lib",
-});
+export default defineConfig([
+	{
+		clean: true,
+		dts: true,
+		entry: ["src/index.ts"],
+		format: ["cjs", "esm"],
+		outDir: "lib",
+	},
+	{
+		dts: false,
+		entry: ["src/bin/pjv.ts"],
+		external: ["package-json-validator"],
+		format: ["esm"],
+		outDir: "lib/bin",
+	},
+]);
