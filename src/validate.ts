bf032999bb4087047ccad70386058b08402517d1
michael faith <michaelfaith@users.noreply.github.com>
2025-06-15 08:36:50 -0500

    refactor: move supporting functions to `utils`

--- src/validate.ts
+++ src/validate.ts
@@ -10,7 +10,6 @@ import {
 	validateAuthor,
 	validateBin,
 	validateDependencies,
-	validateType,
 	validateUrlOrMailto,
 } from "./validators/index.js";
 
--- src/validators/index.ts
+++ src/validators/index.ts
@@ -1,5 +1,4 @@
 export { validateAuthor } from "./validateAuthor.js";
 export { validateBin } from "./validateBin.js";
 export { validateDependencies } from "./validateDependencies.js";
-export { validateType } from "./validateType.js";
 export { validateUrlOrMailto } from "./validateUrlOrMailto.js";
