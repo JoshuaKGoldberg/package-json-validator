6c30d244f0d0683168b567268367c91116717aee
Brad Jorsch <anomiex@users.noreply.github.com>
2025-07-11 17:48:15 -0400

    feat(validateDependencies): Improve dependency name validation (#339)
    <!-- 👋 Hi, thanks for sending a PR to package-json-validator! 💖
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue:
    https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/issues/1165
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/package-json-validator/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/package-json-validator/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->
    * Tighten recognition of `git` and `github` protocols. Instead of
    anything starting with `git`, look for actual `git:`, `git+ssh:`,
    `github:`, and so on.
    * Recognize [GitHub
    shorthand](https://docs.npmjs.com/cli/v11/configuring-npm/package-json#github-urls).
    * Recognize paths without `file:` prefix.
    * Only enforce npmjs.com package name restrictions for versions that
    look like they'd be resolved from a published package, not e.g.
    workspaces, relative paths, git refs, github refs, or aliases, by
    splitting `isUnpublishedVersion` from `isValidVersionRange` and using
    the former in the check for valid package names.
    This is per request at
    https://github.com/JoshuaKGoldberg/eslint-plugin-package-json/issues/1165#issuecomment-3054338310
    
    💖

--- src/validators/validateDependencies.ts
+++ src/validators/validateDependencies.ts
@@ -1,23 +1,43 @@
 import { packageFormat, urlFormat } from "../formats.js";
 
+const isUnpublishedVersion = (v: string): boolean => {
+	return (
+		// https://docs.npmjs.com/cli/v11/configuring-npm/package-json#urls-as-dependencies
+		urlFormat.test(v) ||
+		// https://docs.npmjs.com/cli/v11/configuring-npm/package-json#git-urls-as-dependencies
+		/^git(?:\+(?:ssh|http|https|file|rsync|ftp))?:/.test(v) ||
+		// https://docs.npmjs.com/cli/v11/configuring-npm/package-json#github-urls
+		/^(?:github:)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*\/[\w.-]+(?:#|$)/.test(v) ||
+		// https://pnpm.io/next/workspaces#workspace-protocol-workspace
+		/^workspace:((\^|~)?[0-9.x]*|(<=?|>=?)?[0-9.x][\-.+\w]+|\*)?$/.test(v) ||
+		// https://docs.npmjs.com/cli/v11/using-npm/package-spec#aliases
+		v.startsWith("npm:") ||
+		// https://docs.npmjs.com/cli/v10/configuring-npm/package-json#local-paths
+		v.startsWith("file:") ||
+		v.startsWith("../") ||
+		v.startsWith("~/") ||
+		v.startsWith("./") ||
+		v.startsWith("/") ||
+		false
+	);
+};
+
 const isValidVersionRange = (v: string): boolean => {
-	// https://github.com/isaacs/npm/blob/master/doc/cli/json.md#dependencies
+	// https://docs.npmjs.com/cli/v11/configuring-npm/package-json#dependencies
 	return (
 		/^[\^<>=~]{0,2}[0-9.x]+/.test(v) ||
-		urlFormat.test(v) ||
 		v == "*" ||
 		v === "" ||
 		v === "latest" ||
-		v.startsWith("git") ||
-		// https://pnpm.io/next/workspaces#workspace-protocol-workspace
-		/^workspace:((\^|~)?[0-9.x]*|(<=?|>=?)?[0-9.x][\-.+\w]+|\*)?$/.test(v) ||
-		// https://pnpm.io/next/catalogs
-		v.startsWith("catalog:") ||
-		v.startsWith("npm:") ||
 		// https://jsr.io/docs/using-packages
 		v.startsWith("jsr:") ||
-		// https://docs.npmjs.com/cli/v10/configuring-npm/package-json#local-paths
-		v.startsWith("file:") ||
+		// https://pnpm.io/next/catalogs
+		// These could be published or unpublished. Ideally linting would validate the
+		// catalog itself, and then we could ignore the package names here since they'd be
+		// correctly validated there. But the catalog is elsewhere, and the better
+		// assumption is that it mostly has published packages.
+		v.startsWith("catalog:") ||
+		isUnpublishedVersion(v) ||
 		false
 	);
 };
@@ -35,7 +55,10 @@ export const validateDependencies = (
 ): string[] => {
 	const errors: string[] = [];
 	for (const pkg in deps) {
-		if (!packageFormat.test(pkg)) {
+		if (
+			!packageFormat.test(pkg) &&
+			!(typeof deps[pkg] === "string" && isUnpublishedVersion(deps[pkg]))
+		) {
 			errors.push(`Invalid dependency package name: ${pkg}`);
 		}
 
