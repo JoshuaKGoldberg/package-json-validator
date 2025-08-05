78e935dcbe27df94edbe1d800fb890097ad0ec94
michael faith <michaelfaith@users.noreply.github.com>
2025-08-05 05:43:57 -0500

    feat(validateDescription): add function for validating `description` (#381)
    <!-- 👋 Hi, thanks for sending a PR to package-json-validator! 💖
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #315 
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/package-json-validator/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/package-json-validator/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    This change adds a new `validateDescription` function that validates the
    value of the "description" field of a package.json. It returns a list of
    error messages if a violation is found. Validation of `description` in
    the monolithic validate function has been switched over to use this
    function as well. That means the criteria for `description`, when
    running `validate`, is stricter.
    
    The new function validates that the value is a non-empty string.

---
+++ src/validators/validateDescription.test.ts
@@ -0,0 +1,57 @@
+import { describe, expect, it } from "vitest";
+
+import { validateDescription } from "./validateDescription.js";
+
+describe("validateDescription", () => {
+	it("should return no errors for a string", () => {
+		expect(validateDescription("The Fragile")).toEqual([]);
+	});
+
+	it("should return error if the value is not a string (number)", () => {
+		expect(validateDescription(123)).toEqual([
+			"the type should be a `string`, not `number`",
+		]);
+	});
+
+	it("should return error if the value is not a string (object)", () => {
+		expect(validateDescription({})).toEqual([
+			"the type should be a `string`, not `object`",
+		]);
+	});
+
+	it("should return error if the value is not a string (Array)", () => {
+		expect(validateDescription([])).toEqual([
+			"the type should be a `string`, not `Array`",
+		]);
+	});
+
+	it("should return error if value is not a string (boolean)", () => {
+		expect(validateDescription(true)).toEqual([
+			"the type should be a `string`, not `boolean`",
+		]);
+	});
+
+	it("should return error if value is not a string (undefined)", () => {
+		expect(validateDescription(undefined)).toEqual([
+			"the type should be a `string`, not `undefined`",
+		]);
+	});
+
+	it("should return error if value is not a string (null)", () => {
+		expect(validateDescription(null)).toEqual([
+			"the field is `null`, but should be a `string`",
+		]);
+	});
+
+	it("should return error if the value is an empty string", () => {
+		expect(validateDescription("")).toEqual([
+			"the value is empty, but should be a description",
+		]);
+	});
+
+	it("should return error if the value is whitespace only", () => {
+		expect(validateDescription("   ")).toEqual([
+			"the value is empty, but should be a description",
+		]);
+	});
+});
