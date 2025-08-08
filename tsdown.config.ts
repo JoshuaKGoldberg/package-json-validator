c76882af62f9b17d59a3c001d476fb230c667d32
michael faith <michaelfaith@users.noreply.github.com>
2025-08-08 03:54:27 -0500

    remove redundant settings

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-	clean: true,
-	dts: true,
 	entry: ["src/**/*.ts", "!src/**/*.test.*"],
-	format: ["esm"],
 	outDir: "lib",
 	unbundle: true,
 });
