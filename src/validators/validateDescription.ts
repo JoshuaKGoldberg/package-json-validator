91af809968b0abd4c215d9aeb0cd7371ffe33ed2
michael faith <michaelfaith@users.noreply.github.com>
2025-11-03 12:53:28 -0600

    feat(validateDescription): adopt new Result return type (#522)
    <!-- 👋 Hi, thanks for sending a PR to package-json-validator! 💖
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #478 
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/package-json-validator/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/package-json-validator/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Following the new design decided on in #393, this change updates
    `validateDescription` to adopt the new Result return type.

--- src/validators/validateDescription.ts
+++ src/validators/validateDescription.ts
@@ -1,20 +1,22 @@
+import { Result } from "../Result.ts";
+
 /**
  * Validate the `description` field in a package.json.
  * It should be a non-empty string..
  */
-export const validateDescription = (value: unknown): string[] => {
-	const errors: string[] = [];
+export const validateDescription = (value: unknown): Result => {
+	const result = new Result();
 
 	if (typeof value !== "string") {
 		if (value === null) {
-			errors.push("the field is `null`, but should be a `string`");
+			result.addIssue("the value is `null`, but should be a `string`");
 		} else {
 			const valueType = Array.isArray(value) ? "Array" : typeof value;
-			errors.push(`the type should be a \`string\`, not \`${valueType}\``);
+			result.addIssue(`the type should be a \`string\`, not \`${valueType}\``);
 		}
 	} else if (value.trim() === "") {
-		errors.push("the value is empty, but should be a description");
+		result.addIssue("the value is empty, but should be a description");
 	}
 
-	return errors;
+	return result;
 };
